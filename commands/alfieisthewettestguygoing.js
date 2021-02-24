d63f15071c1ba4d8253739904e5fe645153781a8
IPHONE2356 <73042876+IPHONE2356@users.noreply.github.com>
2021-02-24 17:45:57 +0000

    24_02_21 2
    -Updated alfieisthewettestguygoing
    -Updated crustylips

--- commands/alfieisthewettestguygoing.js
+++ commands/alfieisthewettestguygoing.js
@@ -3,28 +3,28 @@ module.exports = {
 	description:'null',
 	execute(message, args) {
 		if(message.author.id == 637640082953601044) {
-			message.channel.send(`Dear gayfag Alfie,
+			message.channel.send(`**Dear gayfag Alfie,
 The fact that u think u have the superiority to insult saka amazes me. Ur breath kicks on multiple different levels and u sound like a fucking year 5 not to mention 
 u would probably get raped by one, furthermore u literally take ur cat on fucking walks u gayfag Jew prick nonce I hope u break both ur legs
 Yours sincerely ur master
 U defo eat raw pasty chicken
-On a regular`);
+On a regular**`);
 		}
 		else if (message.author.tag == 'Banter Gaming#1624') {
-			message.channel.send(`Dear gayfag Alfie,
+			message.channel.send(`**Dear gayfag Alfie,
 The fact that u think u have the superiority to insult saka amazes me. Ur breath kicks on multiple different levels and u sound like a fucking year 5 not to mention 
 u would probably get raped by one, furthermore u literally take ur cat on fucking walks u gayfag Jew prick nonce I hope u break both ur legs
 Yours sincerely ur master
 U defo eat raw pasty chicken
-On a regular`);
+On a regular**`);
 		}
 		else if (message.author.id == 508345920639729675) {
-			message.channel.send(`Dear gayfag Alfie,
+			message.channel.send(`**Dear gayfag Alfie,
 The fact that u think u have the superiority to insult saka amazes me. Ur breath kicks on multiple different levels and u sound like a fucking year 5 not to mention 
 u would probably get raped by one, furthermore u literally take ur cat on fucking walks u gayfag Jew prick nonce I hope u break both ur legs
 Yours sincerely ur master
 U defo eat raw pasty chicken
-On a regular`);
+On a regular**`);
 		}
 		else{
 			message.channel.send(`You do not have access to this command ${message.author}`);
--- commands/crustylips.js
+++ commands/crustylips.js
@@ -3,31 +3,31 @@ module.exports = {
 	description:"null",
 	execute(message,args){
 		if(message.author.id == 637640082953601044){
-		message.channel.send(`To stinky breath fish
+		message.channel.send(`**To stinky breath fish
 Idk why u mock saka as he will be better than any of ur players by a country mile
 However I have noticed how ashy ur knees are
 They look like u constantly fall over and graze them
 Aswell as ur elbows
 And that huge fookin forehead is a different story
-And I will just brush upon how u slurp Brian’s smegma and most likely have thrush`)
+And I will just brush upon how u slurp Brian’s smegma and most likely have thrush**`)
 		}
 		else if(message.author.tag == "Banter Gaming#1624"){
-		message.channel.send(`To stinky breath fish
+		message.channel.send(`**To stinky breath fish
 Idk why u mock saka as he will be better than any of ur players by a country mile
 However I have noticed how ashy ur knees are
 They look like u constantly fall over and graze them
 Aswell as ur elbows
 And that huge fookin forehead is a different story
-And I will just brush upon how u slurp Brian’s smegma and most likely have thrush`)
+And I will just brush upon how u slurp Brian’s smegma and most likely have thrush**`)
 		}
 		else if(message.author.id == 508345920639729675){
-		message.channel.send(`To stinky breath fish
+		message.channel.send(`**To stinky breath fish
 Idk why u mock saka as he will be better than any of ur players by a country mile
 However I have noticed how ashy ur knees are
 They look like u constantly fall over and graze them
 Aswell as ur elbows
 And that huge fookin forehead is a different story
-And I will just brush upon how u slurp Brian’s smegma and most likely have thrush`)
+And I will just brush upon how u slurp Brian’s smegma and most likely have thrush**`)
 		
 		}	
 		else{

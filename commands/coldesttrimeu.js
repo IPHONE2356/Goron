8b4c6401b907d332676e1260da50445f671d300e
IPHONE2356 <73042876+IPHONE2356@users.noreply.github.com>
2021-02-26 15:10:50 +0000

    26_02_2021 5

---
+++ commands/coldesttrimeu.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name:"coldesttrimeu",
+	description:"Best trim in the EU - Requested by alfie",
+	execute(message,args){
+		message.channel.send({files: ["besttrim.png"]})
+	}
+}

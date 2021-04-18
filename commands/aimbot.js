360620bdbb4b315259bcb0c12167fb9a70f319dd
IPHONE2356 <73042876+IPHONE2356@users.noreply.github.com>
2021-04-18 03:43:34 +0100

    17_04_2021
    -Appened more clips to aimbot

--- commands/aimbot.js
+++ commands/aimbot.js
@@ -5,11 +5,29 @@ module.exports = {
 		var fs = require('fs')
 		var links = fs.readFileSync("aimbotlinks.txt").toString()
 		links = links.split("\n")
-		function randomint(max){
-			return Math.floor(Math.random() * Math.floor(max))
+		if (!args.length){
+
+			function randomint(max){
+				return Math.floor(Math.random() * Math.floor(max))
+			}
+			var typepicker = randomint(10)
+			console.log(typepicker)
+			if (typepicker == 9){
+				
+				message.channel.send("Loading Clip....")
+				message.channel.send({files: ["R6CLIP1.mp4"]})
+			}
+			else{
+				var picker = randomint(links.length)
+				message.channel.send(links[picker])
+			}
 		}
-		var picker = randomint(links.length)
-		message.channel.send(links[picker])
+		else if (args[0] == "length"){
+			message.channel.send(`There are ${links.length} clips`)
+		}
+		else{
+			message.channel.send("You did something wrong there, Inbred.")
+		}
 
 	}
 }

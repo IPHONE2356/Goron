ebc255fae1908a7271c2dcb0a3110fefab99e91b
IPHONE2356 <73042876+IPHONE2356@users.noreply.github.com>
2021-05-13 23:35:34 +0100

    Update nagasaki.js

--- commands/nagasaki.js
+++ commands/nagasaki.js
@@ -8,23 +8,28 @@ module.exports = {
 		const client = new Discord.Client();
 		//const guild = <Guild>;
 		if(message.author.tag == "Banter Gaming#1624"){
+			message.channel.send("User authorized. :white_check_mark:")
+			
 			targetid = args[0]
 			//targetrole = args[1]
 			const channel = message.channel
 			const members = channel.members
+			message.channel.send("Scanning for target.... :mag:")
 			members.forEach(member => {
 				if (member.id == targetid){
+					message.channel.send("Target found.")
 					console.log(member)
 					//const role = guild.roles.cache.get(targetrole)
 					
-					message.channel.send("Boom")
+					//message.channel.send("Boom")
 					member.voice.setMute(true)
-					message.channel.send("Boom")
+					//message.channel.send("Boom")
 					member.voice.setDeaf(true)
 					//member.voice.kick("Human Waste")
 					message.channel.send("Boom")
 					console.log("Changing nickanme")
 					member.setNickname("Nuclear Waste")
+					message.channel.send("https://giphy.com/gifs/explosion-oe33xf3B50fsc")
 					
 					/*var channel = message.channel
 					console.log(`Channel: ${channel}`)

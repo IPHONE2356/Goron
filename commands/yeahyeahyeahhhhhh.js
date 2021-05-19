bb3765598863b207d4faae8d67cd56a9c145eef1
IPHONE2356 <sasadiq1@icloud.com>
2021-05-19 19:44:08 +0100

    Updated yeahyeahyeahhhhhh.js

--- commands/yeahyeahyeahhhhhh.js
+++ commands/yeahyeahyeahhhhhh.js
@@ -1,34 +1,100 @@
 module.exports = {
 	name:"yeahyeahyeahhhhhh",
 	description:"yeah yeah yeahhhhhh",
+	aliases:["bom","do","msm"],
 	async execute(message,args){
-		const Discord = require('discord.js')
-		var connection = await message.member.voice.channel.join()
-		
-		const dispatcher = connection.play('msmsong.mp3', {volume: 0.5});
-
-		dispatcher.on('start', () => {
-			console.log('msmsong.mp3 is now playing!');
-			message.channel.send("Now playing msmsong.mp3")
-			const fs = require('fs');
-
-// Create a ReadableStream of s16le PCM audio
-			//const audio = connection.receiver.createStream(message.author, { mode: 'pcm', end: "manual"});
-
-
-			//counter = counter + 1
-		});
-		console.log("Start dispatch")
-		dispatcher.on('finish', () => {
-			console.log('msmsong.mp3 has finished playing!');
-			message.channel.send("msmsong.mp3 has stopped playing, Disconnecting. ")
-			//message.channel.send("Error disconnecting, ``voiccechannel is not defined``")
-			connection.disconnect()
+		if(!args.length){
+			const Discord = require('discord.js')
+			var connection = await message.member.voice.channel.join()
 			
-		});
+			const dispatcher = connection.play('msmsong.mp3', {volume: 0.35});
 
-		// Always remember to handle errors appropriately!
-		dispatcher.on('error', console.error);
+			dispatcher.on('start', () => {
+				console.log('msmsong.mp3 is now playing!');
+				message.channel.send("Now playing msmsong.mp3")
+				const fs = require('fs');
+
+	// Create a ReadableStream of s16le PCM audio
+				//const audio = connection.receiver.createStream(message.author, { mode: 'pcm', end: "manual"});
+
+
+				//counter = counter + 1
+			});
+			console.log("Start dispatch")
+			dispatcher.on('finish', () => {
+				console.log('msmsong.mp3 has finished playing!');
+				message.channel.send("msmsong.mp3 has stopped playing, Disconnecting. ")
+				//message.channel.send("Error disconnecting, ``voiccechannel is not defined``")
+				connection.disconnect()
+				
+			});
+
+
+			// Always remember to handle errors appropriately!
+			dispatcher.on('error', console.error);
+		}
+		else if(args[0] == "shugabush"){
+
+			const Discord = require('discord.js')
+			var connection = await message.member.voice.channel.join()
+			
+			const dispatcher = connection.play('shugabush.mp3', {volume: 0.35});
+
+			dispatcher.on('start', () => {
+				console.log('shugabush.mp3 is now playing!');
+				message.channel.send("Now playing shugabush.mp3")
+				const fs = require('fs');
+
+	// Create a ReadableStream of s16le PCM audio
+				//const audio = connection.receiver.createStream(message.author, { mode: 'pcm', end: "manual"});
+
+
+				//counter = counter + 1
+			});
+			console.log("Start dispatch")
+			dispatcher.on('finish', () => {
+				console.log('shugabush.mp3 has finished playing!');
+				message.channel.send("shugabush.mp3 has stopped playing, Disconnecting. ")
+				//message.channel.send("Error disconnecting, ``voiccechannel is not defined``")
+				connection.disconnect()
+				
+			});
+
+
+			// Always remember to handle errors appropriately!
+			dispatcher.on('error', console.error);
+
+		}
+		else if(args[0] == "shugaisland"){
+			const Discord = require('discord.js')
+			var connection = await message.member.voice.channel.join()
+			
+			const dispatcher = connection.play('shugabushfull.mp3', {volume: 0.35});
+
+			dispatcher.on('start', () => {
+				console.log('shugabushfull.mp3 is now playing!');
+				message.channel.send("Now playing shugabushfull.mp3")
+				const fs = require('fs');
+
+	// Create a ReadableStream of s16le PCM audio
+				//const audio = connection.receiver.createStream(message.author, { mode: 'pcm', end: "manual"});
+
+
+				//counter = counter + 1
+			});
+			console.log("Start dispatch")
+			dispatcher.on('finish', () => {
+				console.log('shugabushfull.mp3 has finished playing!');
+				message.channel.send("shugabushfull.mp3 has stopped playing, Disconnecting. ")
+				//message.channel.send("Error disconnecting, ``voiccechannel is not defined``")
+				connection.disconnect()
+				
+			});
+
+
+			// Always remember to handle errors appropriately!
+			dispatcher.on('error', console.error);
+		}
 
 	}
 }

0f009c34d809f207f8c4d06cfff50ce4dab79e5f
IPHONE2356 <73042876+IPHONE2356@users.noreply.github.com>
2021-06-02 21:07:13 +0100

    2_06_2021
    -Added jaiminsknees

---
+++ commands/jaiminsknees.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name:"jaiminsknees",
+	description:"Dry dry dry",
+	aliases:["sahara","desert","ash","schoolburgers"],
+	execute(message,args){
+		message.channel.send({files:["jaiminsknees.png"]})
+	}
+}

7648100e3819091a6919fe2646b0ec2a4ab22797
IPHONE2356 <73042876+IPHONE2356@users.noreply.github.com>
2021-06-02 20:58:39 +0100

    2_06_2021
    -Added jaiminsmoisturiser

---
+++ commands/jaiminsmoisturiser.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name:"jaiminsmoisturiser",
+	description:"Dry dry dry ",
+	aliases:["dry","jiminim","moisturiser","cream"],
+	execute(message,args){
+		message.channel.send({files:["sandpaper.png"]})
+	}
+}

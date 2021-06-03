4456b2ba7637805a40d39992c5b6697b692e1584
IPHONE2356 <73042876+IPHONE2356@users.noreply.github.com>
2021-06-03 02:42:11 +0100

    3_06_2021
    -Updated bricker

--- commands/bricker.js
+++ commands/bricker.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name:"bricker",
 	description:"Brick brick brick briiick",
+	aliases:["bricka","bricks"]
 	execute(message,args){
 		message.channel.send({files:["bricker.png"]})
 	}

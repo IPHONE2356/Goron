7fa817a0d86eaae3c503dd29b91b80a4dc215dec
IPHONE2356 <73042876+IPHONE2356@users.noreply.github.com>
2021-06-05 01:59:31 +0100

    5_06_2021
    -Added dylan apex
    -Added suli

---
+++ commands/dylanapex.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name:"dylanapex",
+	description:"Dylans first dub on apex and he actually carried",
+	execute(message,args){
+		message.channel.send({files:["dylanapex.png"]})
+	}
+}
---
+++ commands/suli.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name:"suli",
+	description:"Suli check",
+	execute(message,args){
+		message.channel.send("https://tenor.com/view/suli-check-gif-20096728")
+	}
+}

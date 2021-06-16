06fca9fa85cd4617196dc59a029308226abcca2f
IPHONE2356 <73042876+IPHONE2356@users.noreply.github.com>
2021-06-16 23:04:47 +0100

    Create yournotthatguy.js

---
+++ commands/yournotthatguy.js
@@ -0,0 +1,7 @@
+moudule.exports = {
+	name:"yournotthatguy",
+	description:"You're not that guy pal. Trust me, You're not that guy",
+	execute(message,args){
+		message.channel.send{files:["yntgp.mp4"]}
+	}
+}

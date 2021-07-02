49379e2d3abb0a80e415524900b3d6e9a0047eda
IPHONE2356 <73042876+IPHONE2356@users.noreply.github.com>
2021-07-02 01:42:51 +0100

    Update bricker.js

--- commands/bricker.js
+++ commands/bricker.js
@@ -5,5 +5,10 @@ module.exports = {
 	execute(message,args){
 		message.channel.send("RIP Bricka")
 		message.channel.send({files:["bricker.png"]})
+		message.channel.bulkDelete(2,true).catch(err => {
+				console.error(err)
+				message.channel.send("Error, jaimin stinks")
+			});
+		message.channel.send("I lieeeeeeed")
 	}
 }

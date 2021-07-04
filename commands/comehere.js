9626b95877b67ae2ed8c7a290066b331e653f530
IPHONE2356 <73042876+IPHONE2356@users.noreply.github.com>
2021-07-04 21:34:13 +0100

    Update comehere.js

--- commands/comehere.js
+++ commands/comehere.js
@@ -8,25 +8,30 @@ module.exports = {
 		//               Banter Gaming      JayzUnited         JustNotVeryGood   
 		var superuser = [201043573162901504,330411095959273482,269179454935400448]
 		var author = message.author.id
+		//Checks to see if the user is authorized
 		superuser.forEach(selectedSu => {
 			if(selectedSu == author){
 				console.log("User authorized")
 				superauth = true
 			}
 		})
+		//Gets the right voice channel
 		const channel = message.channel
 		const members = channel.members
 		if(!args.length){
 			console.log("No arguments provided.")
 			if(superauth == true){
+				//Checks to see if the target user is trying to unmute themselves
 				if(message.author.id == 493402498388721670){
 					message.channel.send({files:["cryingface.mp4"]})
 				}
 				else{
 				console.log("Executing Superuser commnad")
+				//Scans for the target
 				members.forEach(member => {
 					var target = 493402498388721670
 					if(member.id == target){
+						//Unmutes the target and sends a confirmation message
 						member.voice.setMute(false)
 						const embed = new Discord.MessageEmbed()
 						.setColor("00A6FF")
@@ -47,6 +52,7 @@ module.exports = {
 				message.channel.send(embed)
 			}
 		}
+		//For the acapella command
 		else if (args[0] == "acapella"){
 			var target = 460455100092252170
 			if(message.author != target){
@@ -60,12 +66,14 @@ module.exports = {
 					}
 				})
 			}
+			//Checks to see if the target is trying to unmute themselves
 			else if(message.author == target){
 				message.channel.send("Harrison harrison harrison harrison harrison harrison")
 			}
 		}
 		else{
 			if(superauth == true){
+				//Checks to see if the target user is trying to unmute themselves
 				var input = args[0]
 				var target = input.slice(3,-1)
 			 	if(message.author.id != superuser[0] && message.author.id == target){

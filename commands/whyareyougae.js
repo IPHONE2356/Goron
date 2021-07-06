c3a055b925b05e43846552a26770665b3089823e
IPHONE2356 <73042876+IPHONE2356@users.noreply.github.com>
2021-07-06 17:33:00 +0100

    Added whyareyougae

---
+++ commands/whyareyougae.js
@@ -0,0 +1,25 @@
+module.exports = {
+	name:"whyareyougae",
+	description:"Why are you gay? Who says im gay? You are gay, You are transgenda",
+	aliases:["whyareyougay","pepejulianonzima","pastasempa"],
+	execute(message,args){
+		var connection = await message.member.voice.channel.join()
+	
+		const dispatcher = connection.play('whyareyougae.mp3', {volume: 1.5});
+
+		dispatcher.on('start', () => {
+			console.log('Broadcasting whyareyougae.mp3');
+			message.channel.send("Playing whyareyougae.mp3")
+		});
+		console.log("Start dispatch")
+		dispatcher.on('finish', () => {
+			console.log('Playing whyareyougae.mp3');
+			message.channel.send("whyareyougae.mp3 has stopped playing, Disconnecting\n RIP Bricka")
+			connection.disconnect()
+			
+		});
+
+		// Always remember to handle errors appropriately!
+		dispatcher.on('error', console.error);
+	}
+}

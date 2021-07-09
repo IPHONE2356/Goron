416247a7e3e1896b643a0aa0be56237df693c5b5
IPHONE2356 <73042876+IPHONE2356@users.noreply.github.com>
2021-07-09 03:00:13 +0100

    Update bomb.js

--- commands/bomb.js
+++ commands/bomb.js
@@ -16,7 +16,7 @@ module.exports = {
 			if (isNaN(amount)) {
 				return message.reply("That is not a valid number")
 			}
-			else if (amount > 51) {
+			else if (amount > 100) {
 				return message.reply("The maximum amount of messages you can bulk delete is 20")
 			}
 			else {

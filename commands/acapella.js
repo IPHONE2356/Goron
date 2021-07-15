f3b61ad36d0923d1f946222a94c57fcdeb1e63ea
IPHONE2356 <73042876+IPHONE2356@users.noreply.github.com>
2021-07-15 19:50:38 +0100

    Update acapella.js

--- commands/acapella.js
+++ commands/acapella.js
@@ -9,7 +9,7 @@ module.exports = {
 		}
 		else{
 			const channel = message.channel
-			const members = channel.member
+			const members = channel.members
 			var target = 460455100092252170 
 			members.forEach(member => {
 				if(member.id == target){

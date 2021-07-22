258c8a74cf788fdd2166f34ca494f64d2a3f8630
IPHONE2356 <73042876+IPHONE2356@users.noreply.github.com>
2021-07-22 16:02:41 +0100

    It seems anyone can access the bots token using the config command, a security breach waiting to happen.

--- commands/config.js
+++ commands/config.js
@@ -10,20 +10,27 @@ module.exports = {
 				message.channel.send(`No arguments were provided, ${message.author}`)
 			}
 			*/
-			if (args[0] == "token"){
-				message.channel.send(config.token)
+			if (args[0] == "token") {
+				if (message.author.tag == "Banter Gaming#1624") {
+					message.channel.send(config.token)
+				}
+				else {
+					message.channel.send("Tokens are just passwords but longer, and if they get into the wrong hands the bot can be hijacked and any code can be executed from it. Only the bot owner has access to this feature of the command")
+                }
 			}
-			else if (args[0] == "testbot"){
-				message.channel.send(config.testbot)
+			else if (args[0] == "testbot") {
+				if (message.author.tag == "Banter Gaming#1624") {
+					message.channel.send(config.testbot)
+				}
+				else {
+					message.channel.send("Tokens are just passwords but longer, and if they get into the wrong hands the bot can be hijacked and any code can be executed from it. Only the bot owner has access to this feature of the command")
+                }
 			}
 			else if (args[0] == "prefix"){
 				message.channel.send(config.prefix)
 			}
 			else if (args[0] == "status"){
 				message.channel.send(config.status)
-			}
-			else if(args[0] == "loadedCommands"){
-				message.channel.send(databank.commandFiles)
 			}
 		}
 	}
